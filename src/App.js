fe86cbf40e6db83700ea355d515e017c2380cb8b
jernber <oraclemars@gmail.com>
2021-02-19 19:13:14 -0600

    created component index, and added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Products from './components/Products/Products'
-import NavBar from './components/Navbar/Navbar'
+
+import { Products, NavBar } from './components'
 
 const App = () => {
     return (
         <div>
+            <NavBar />
            <Products />
         </div>
     )
