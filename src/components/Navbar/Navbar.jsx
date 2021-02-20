db111ec8d5f6ddeb381bf276c25740c99bbdc279
jernber <oraclemars@gmail.com>
2021-02-19 19:07:53 -0600

    added store icon, further progress on navbar, and style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { Appbar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core'
 import { ShoppingCart } from '@material-ui/icons'
-
+import logo from '../../assets/store-icon.png'
 const Navbar = () => {
     return (
         <>
-        
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title} color="inherit">
+                        <img src='' alt='Boreale.js' height="25px" className={classes.image} />
+                        Boreale.js
+                    </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.button}>
+                        <IconButton aria-label="Show cart items" color="inherit">
+                            <Badge badgeContent={2} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
         </>
     )
 }
