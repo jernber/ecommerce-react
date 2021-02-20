a058a083665fbbeb86d4b8ed2a624c1a7bde3d64
jernber <oraclemars@gmail.com>
2021-02-20 13:45:25 -0600

    add to cart functionality added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React, { useState, useEffect } from 'react'
 import { commerce } from './lib/commerce'
 import { Products, Navbar } from './components'
+import { queryByAltText } from '@testing-library/react'
 
 const App = () => {
     const [products, setProducts] = useState([])
+    const [cart, setCart] = useState({})
+
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
 
         setProducts(data)
     }
 
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve())
+    }
+
+    const handleAddToCart = async (productId, qty) => {
+        const item = await commerce.cart.add(productId, qty)
+
+        setCart(item.cart)
+    }
+
     useEffect(() => {
         fetchProducts()
+        fetchCart()
     }, [])
+
+    console.log(cart)
 
     return (
         <div>
             <Navbar />
-            <Products products={products} />
+            <Products products={products} onAddToCart={handleAddToCart} />
         </div>
     )
 }
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -3,10 +3,9 @@ import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } fro
 import { AddShoppingCart } from '@material-ui/icons'
 import useStyles from './styles'
 
-const Product = ({ product }) => {
+const Product = ({ product, onAddToCart }) => {
     const classes = useStyles();
 
-    console.log(product)
     return (
        <Card className={classes.root}>
            <CardMedia className={classes.media} image={product.media.source} title={product.name} />
@@ -20,11 +19,13 @@ const Product = ({ product }) => {
                    </Typography>
                </div>
                <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" />
+
                <CardActions disableSpacing className={classes.CardActions}>
-                   <IconButton aria-label="Add to Cart">
+                   <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)}>
                        <AddShoppingCart />
                    </IconButton>
                </CardActions>
+
            </CardContent>
        </Card>
     )
