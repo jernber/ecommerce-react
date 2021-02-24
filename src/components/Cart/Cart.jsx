64e2a3198b68f3f9ec7db466af3cb38f6b1bbd2b
jernber <oraclemars@gmail.com>
2021-02-24 17:47:21 -0600

    fixed issue where cart was still waiting

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core'
-
+import useStyles from './styles'
 
 const Cart = ({ cart }) => {
-    const isEmpty = !cart.line_items.length 
+    const classes = useStyles()
+
     const EmptyCart = () => (
         <Typography variant="subtitle1">You have no items in your shopping cart. Start adding some!</Typography>
     )
@@ -11,7 +12,7 @@ const Cart = ({ cart }) => {
     const FilledCart = () => (
         <>
             <Grid container spacing={3}>
-                {cart.line_items.map(() => (
+                {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
                         <div>{item.name}</div>
                     </Grid>
@@ -26,11 +27,14 @@ const Cart = ({ cart }) => {
             </div>
         </>
     )
+    // solves fetching of cart
+    if(!cart.line_items) return 'Loading...'
+
     return (
        <Container>
            <div className={classes.toolbar} />
            <Typography className={classes.title} variant="h3">Your Shopping Cart</Typography>
-           {isEmpty? <EmptyCart /> : <FilledCart /> }
+           {!cart.line_items.length? <EmptyCart /> : <FilledCart /> }
        </Container>
     )
 }
