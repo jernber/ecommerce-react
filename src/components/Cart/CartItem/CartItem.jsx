0926cd1f5684898700a8d7c9a13441dc832856f1
jernber <oraclemars@gmail.com>
2021-02-24 17:54:51 -0600

    setup cartitem within cart

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,3 +1,12 @@
 import { Typography, Button, Card, CardActinos, CardContent, CardMedia } from '@material-ui/core'
 
 import useStyles from './styles'
+
+const CartItem = () => {
+    const classes = useStyles()
+    return (
+        <Card>
+            <CardMedia image={image.media.source}/>
+        </Card>
+    )
+}
